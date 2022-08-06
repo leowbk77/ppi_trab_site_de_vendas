4be2738b0280ffab50083e4ed2727b58d59aa76a
Leonardo <leonardo.mf@ufu.br>
2022-08-05 23:37:58 -0300

    update

---
+++ js/loginValidate.js
@@ -0,0 +1,10 @@
+const inputDoEmail = document.getElementById("inputEmail");
+const inputDaSenha = document.getElementById("inputSenha");
+const botaoLogin = document.getElementById("loginbtn");
+
+botaoLogin.addEventListener("click", function validarLogin(){
+    /*  ENVIAR PARA O SERVIDOR A SENHA E EMAIL
+        INFORMAR DINAMICAMENTE SE OS DADOS ESTAO ERRADOS SEM RELOAD
+        CRIAR SESSAO NO LADO DO SERVER CASO LOGIN BEM SUCEDIDO E DAR ACESSO A PARTE RESTRITA
+    */
+});
