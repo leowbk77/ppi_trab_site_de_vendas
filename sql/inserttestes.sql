ed6bb1e0cbb404d0c2179cdf2391aba1a2eb6d12
Leonardo <leonardo.mf@ufu.br>
2022-08-07 16:59:54 -0300

    sql de testes adicionado

---
+++ sql/inserttestes.sql
@@ -0,0 +1,73 @@
+INSERT INTO anunciante (nome, cpf, email, senha_hash, telefone)
+VALUES ('fulano', '12312312323', 'fulanoum@email.com', '$2y$10$8hvE9JK8cB4o2UhjvbLLRew2ncFWePqM9RD.Ka3gIJ6jTsuWoHgga', '38310000'); -- senha coxinha123
+
+INSERT INTO anunciante (nome, cpf, email, senha_hash, telefone)
+VALUES ('fulanoDOis', '12345678912', 'fulanodois@email.com', '$2y$10$l9vZ5vPT/BJVhuJW9DMwmeKKMYhsIqhzF3Vvqqedz8OOkAs/hAlqm', '38311245'); -- senha coxinha321
+
+INSERT INTO categoria (nome, descricao)
+VALUES ('categoria um', 'primeira categoria de teste');
+
+INSERT INTO categoria (nome, descricao)
+VALUES ('categoria dois', 'segunda categoria de teste');
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste um', 'descricao do teste um', 13.5, 'cepum', 'bairroUm', 'CidadeUm', 'EstadoUm', 1, 1);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste dois', 'descricao do teste dois', 13.5, 'cepdois', 'bairroDois', 'CidadeDois', 'EstadoDois', 1, 1);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste tres', 'descricao do teste tres', 13.5, 'ceptres', 'bairroTres', 'CidadeTres', 'EstadoTres', 1, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste quatro', 'descricao do teste quatro', 13.5, 'cepquatro', 'bairroQuatro', 'CidadeQuatro', 'EstadoQuatro', 1, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste cinco', 'descricao do teste cinco', 13.5, 'cepcinco', 'bairroCinco', 'CidadeCinco', 'EstadoCinco', 2, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste seis', 'descricao do teste seis', 13.5, 'cepseis', 'bairroSeis', 'CidadeSeis', 'EstadoSeis', 2, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste sete', 'descricao do teste sete', 13.5, 'cepsete', 'bairroSete', 'CidadeSete', 'EstadoSete', 2, 1);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste oito', 'descricao do teste oito', 13.5, 'cepoito', 'bairroOito', 'CidadeOito', 'EstadoOito', 2, 1);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste nove', 'descricao do teste nove', 13.5, 'cepnove', 'bairroNove', 'CidadeNove', 'EstadoNove', 1, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste dez', 'descricao do teste dez', 13.5, 'cepdez', 'bairroDez', 'CidadeDez', 'EstadoDez', 1, 2);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste onze', 'descricao do teste onze', 13.5, 'ceponze', 'bairroOnze', 'CidadeOnze', 'EstadoOnze', 2, 1);
+
+INSERT INTO anuncio (titulo, descricao, preco, cep, bairro, cidade, estado, codigo_categoria, codigo_anunciante)
+VALUES ('Teste doze', 'descricao do teste doze', 13.5, 'cepdoze', 'bairroDoze', 'CidadeDoze', 'EstadoDoze', 2, 1);
+
+
+INSERT INTO foto
+VALUES (1, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (2, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (3, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (4, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (5, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (6, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (7, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (8, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (9, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (10, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (11, 'http://www.hellasconstructions.com/page-under-construction.jpg');
+INSERT INTO foto
+VALUES (12, 'http://www.hellasconstructions.com/page-under-construction.jpg');
